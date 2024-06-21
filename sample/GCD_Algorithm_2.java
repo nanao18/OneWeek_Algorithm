12b75b78b220d294d01c1ef67e42bea2fb2d753d
nanao18 <masuworkpg@gmail.com>
2024-06-21 14:58:29 +0900

    最大公約数

---
+++ sample/GCD_Algorithm_2.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class GCD_Algorithm_2 {
+
+	// Find the greatest common divisor
+	// 引き算の繰り返しで最大公約数を求めるプログラム
+	public static void main(String[] args) {
+	    int m = 876; //1つめの数字
+	    int n = 204;  //2つめの数字
+	
+	    int l = m;
+	    int s = n;
+		while(true){
+			if(l > s) {
+				l = l - s;
+			} else if (s > l) {
+				s = s - l;
+			} else {
+				break;
+			}
+		}
+		System.out.println(m +"と"+ n +"の最大公約数は、"+ l);
+	}
+}
