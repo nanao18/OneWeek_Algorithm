dfae62f5dbebd5a683c02ac3f569a9af27c45e61
nanao18 <masuworkpg@gmail.com>
2024-06-24 13:53:00 +0900

    素因数分解

--- primeFactorization/Sample_1.java
+++ primeFactorization/Sample_1.java
@@ -5,15 +5,23 @@ import java.util.ArrayList;
 public class Sample_1 {
 
 	// 素因数分解を求める (Find all prime factorization)
+	// 試し割り法(Trial division)
 	public static void main(String[] args) {
 	    int n = 100;
 		
-	    ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
-	    for(ArrayList<Integer> li : list){
-	        for(int j = 0; j < li.size(); j++){
-	        	System.out.println(list.get(j));
-	        }
-	    }
+		ArrayList<Integer> prime_factors = new ArrayList<Integer>();
+		for(int i=2; i*i<=n; i++) {	
+			while(n % i == 0) {
+				prime_factors.add(i);
+				n = n / i;
+			}
+		}
+		if(n > 1) {
+			prime_factors.add(n);
+		}
+		for(Integer num: prime_factors) {
+			System.out.print(num + " ");
+		}
 	}
 	
 }
